b5213fa1b23995b3118cf508a582833d20b27d2e
anirudh-os <anirudhsripada05@gmail.com>
2024-10-15 10:01:13 +0530

    Added the link to the signin button

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,7 @@ document.getElementById("hamburger").addEventListener("click", function () {
 document.getElementById("closebtn").addEventListener("click", function () {
     document.getElementById("sidebar").style.width = "0";
 });
+
+document.getElementById("sign_in").addEventListener("click", function () {
+    window.location.href = "../webpages/Sign_in_page.html";
+})
